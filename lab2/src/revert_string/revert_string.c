73065418fd48d83d4d5f5297747d33088ea339e3
ChiKApuMbA <105646509+ChiKApuMbA@users.noreply.github.com>
2023-09-11 16:10:49 +0000

    add some files for lr-2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,18 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    char *start = str; // Указатель на начало строки
+    char *end = str + strlen(str) - 1; // Указатель на конец строки
+
+    // Переставляем символы до тех пор, пока указатели не пересекутся
+    while (start < end)
+    {
+        // Меняем символы местами и сдвигаем указатели
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
 }
 
