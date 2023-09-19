2185e62f815d49606b03d09b3d1a2f948f1ce623
ChiKApuMbA <105646509+ChiKApuMbA@users.noreply.github.com>
2023-09-19 21:55:59 +0000

    add some files for lr3

--- lab3/src/find_min_max.c
+++ lab3/src/find_min_max.c
@@ -4,9 +4,22 @@
 
 struct MinMax GetMinMax(int *array, unsigned int begin, unsigned int end) {
   struct MinMax min_max;
-  min_max.min = INT_MAX;
-  min_max.max = INT_MIN;
+  min_max.min = INT_MIN;
+  min_max.max = INT_MAX;
+  if (begin > end || begin >= sizeof(array) / sizeof(int)) {
+    min_max.min = INT_MIN;
+    min_max.max = INT_MAX;
+    return min_max;
+  }
 
-  // your code here
+  for (unsigned int i = begin; i < end; i++) {
+    if (array[i] > min_max.min || i == begin) {
+      min_max.min = array[i];
+    }
+    else if(array[i] < min_max.max || i == begin)
+    {
+      min_max.max = array[i];
+    }
+  }
   return min_max;
 }
